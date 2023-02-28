e3a6ed9c14866d3a340353bf12c999a939633490
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-02-28 13:09:33 +0530

    28 feb

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+// Widget randimg() {
+//   var value = Random().nextInt(5);
+//   value++;
+//   return Image.asset('images/dice${value}.png');
+// }
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    int rightval = 4;
+    int leftval = 2;
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xff4C566A),
         appBar: AppBar(
@@ -24,12 +40,33 @@ class MyApp extends StatelessWidget {
           titleSpacing: 1.5,
         ),
         body: Row(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
-              child: Image.asset('images/dice1.png'),
-            ),
+                flex: 1,
+                child: TextButton(
+                  // style: ButtonStyle(
+                  //   padding: MaterialStateProperty.all(EdgeInsets.zero),
+                  // ),
+                  onPressed: () {
+                    setState(() {
+                      leftval = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice${leftval}.png'),
+                )),
             Expanded(
-              child: Image.asset('images/dice1.png'),
+              flex: 1,
+              child: TextButton(
+                child: Image.asset('images/dice${rightval}.png'),
+                onPressed: () {
+                  setState(() {
+                    rightval = Random().nextInt(6) + 1;
+                  });
+                },
+              ),
             ),
           ],
         ),
